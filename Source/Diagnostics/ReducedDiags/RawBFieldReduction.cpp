534c3204a92cf04f8a61354a8a5430cf3a94fee4
RevathiJambunathan <revanathan@gmail.com>
2023-06-23 17:22:21 -0700

    update variable to m_write_header

--- Source/Diagnostics/ReducedDiags/RawBFieldReduction.cpp
+++ Source/Diagnostics/ReducedDiags/RawBFieldReduction.cpp
@@ -91,7 +91,7 @@ RawBFieldReduction::RawBFieldReduction (std::string rd_name)
 
     if (amrex::ParallelDescriptor::IOProcessor())
     {
-        if ( m_IsNotRestart )
+        if ( m_write_header )
         {
             // open file
             std::ofstream ofs{m_path + m_rd_name + "." + m_extension, std::ofstream::out};
